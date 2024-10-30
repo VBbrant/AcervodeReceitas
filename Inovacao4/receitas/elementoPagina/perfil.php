53c39b5791d68b9be2a842c2879531099d2eafcc
EpaxOfc <mateusdeabreu51@gmail.com>
2024-10-30 10:30:12 -0300

    18

--- Inovacao4/receitas/elementoPagina/perfil.php
+++ Inovacao4/receitas/elementoPagina/perfil.php
@@ -3,6 +3,6 @@
     <ul>
         <li><a href="/profile.php"><i class="fas fa-user"></i> Profile</a></li>
         <li><a href="/settings.php"><i class="fas fa-cog"></i> Settings</a></li>
-        <li><a href="<?php echo ROOT_PATH; ?>receitas/Paginas/Logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+        <li><a href="<?php echo BASE_URL; ?>receitas/Paginas/Logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
     </ul>
 </div>
